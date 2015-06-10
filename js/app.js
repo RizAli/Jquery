0b1389b74f552fa8dbc71e64cd8f10c95d4e1edc
Rizwan Ali <rizcube@gmail.com>
2015-06-10 09:47:35 +0100

    clicking on reveal hides the button and displays spoiler

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Prevent spoilerphobes from seeing spoiler
+// solution: hide spoilers and reveal them through user interaction
+
+// 1, Hide spoiler
+$(".spoiler span").hide();
+
+// 2, Add a button
+$(".spoiler").append("<button>Reveal Spoiler</button>")
+
+// 3, When button pressed
+  // 3.1 Show the spoiler
+  // 3.2 Get rid of button
+$("button").click(function(){
+  $(".spoiler span").slideDown();
+  $(this).remove();
+  $
+});
+
+
+
