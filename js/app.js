1260eace7f436f35935b76191786dff6f206610d
Rizwan Ali <rizcube@gmail.com>
2015-06-13 08:05:52 +0100

    when click on the image it enlarges and when click again it hides

--- js/app.js
+++ js/app.js
@@ -2,19 +2,33 @@
 // Solution: Create an overlay with the large image - lightbox
 
 
-// 1. Capture the click event on a link to an image.
+  //An image
+  //A caption
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+$overlay.append($image);
+
+// Add image to overlay
+$("body").append($overlay);
+
+// Capture the click event on a link to an image.
 $("#imageGallery a").click(function(event){
   event.preventDefault();
-  var href = $(this).attr("href");
-  console.log(href);
+  var imageLocation = $(this).attr("href");
+  //update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+
+  $overlay.show();
+  //Show the overlay
+
+
+  //Get child's alt attribute and set caption.
 });
 
 
-  // 1.1 Show the overlay
-  // 1.2 update overlay with the image linked in the link
-  // 1.3 Get child's alt attribute and set caption.
-// 2. Add overlay
-  //2.1 An image
-  //2.2 A caption
-// 3. When overlay is clicked
-  // 3.1 hide the overlay
+$overlay.click(function(){
+  $overlay.hide();
+});
+
+//When overlay is clicked
+  //hide the overlay
